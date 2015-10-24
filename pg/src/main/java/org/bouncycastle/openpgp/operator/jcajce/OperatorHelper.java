cec9a2fe6ef18436a2b6eae3e41970f6c5f70471
Vincent Breitmoser <valodim@mugenguild.com>
2015-10-24 20:46:34 +0200

    expose signature packet in PGPOnePassSignature, and provide a way to check raw signatures

--- pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/OperatorHelper.java
+++ pg/src/main/java/org/bouncycastle/openpgp/operator/jcajce/OperatorHelper.java
@@ -247,9 +247,23 @@ class OperatorHelper
         }
     }
 
+    public Signature createSignature(int keyAlgorithm)
+            throws PGPException
+    {
+        String encAlg = getAlgorithmName(keyAlgorithm);
+
+        return createSignature("NONE" + "with" + encAlg);
+    }
+
     public Signature createSignature(int keyAlgorithm, int hashAlgorithm)
         throws PGPException
     {
+        String encAlg = getAlgorithmName(keyAlgorithm);
+
+        return createSignature(PGPUtil.getDigestName(hashAlgorithm) + "with" + encAlg);
+    }
+
+    private String getAlgorithmName(int keyAlgorithm) throws PGPException {
         String     encAlg;
 
         switch (keyAlgorithm)
@@ -271,8 +285,7 @@ class OperatorHelper
         default:
             throw new PGPException("unknown algorithm tag in signature:" + keyAlgorithm);
         }
-
-        return createSignature(PGPUtil.getDigestName(hashAlgorithm) + "with" + encAlg);
+        return encAlg;
     }
 
     public AlgorithmParameters createAlgorithmParameters(String algorithm)

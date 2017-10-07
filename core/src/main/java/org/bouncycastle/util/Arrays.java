6e95e0293c11330d77e636dd68d77c2c981bf70e
Josua Mayer <josua.mayer97@gmail.com>
2017-10-07 17:50:39 +0200

    Arrays::copyOfRange: throw exception for negative range
    Throw IllegalArgumentException if from > to (newLength < 0).
    
    Signed-off-by: Josua Mayer <josua.mayer97@gmail.com>

--- core/src/main/java/org/bouncycastle/util/Arrays.java
+++ core/src/main/java/org/bouncycastle/util/Arrays.java
@@ -821,6 +821,11 @@ public final class Arrays
     public static byte[] copyOfRange(byte[] data, int from, int to)
     {
         int newLength = getLength(from, to);
+        if(newLength < 0) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(from).append(" > ").append(to);
+            throw new IllegalArgumentException(sb.toString());
+        }
 
         byte[] tmp = new byte[newLength];
 

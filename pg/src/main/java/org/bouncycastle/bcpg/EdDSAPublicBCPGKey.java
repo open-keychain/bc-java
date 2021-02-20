d920f59594858d2835c330ec95b9dad0c4b7d00c
David Hedberg <david.hedberg@gmail.com>
2021-02-20 19:26:27 +0100

    Add function getEdDSAEncodedPoint from upstream

--- pg/src/main/java/org/bouncycastle/bcpg/EdDSAPublicBCPGKey.java
+++ pg/src/main/java/org/bouncycastle/bcpg/EdDSAPublicBCPGKey.java
@@ -5,6 +5,8 @@ import java.math.BigInteger;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.util.Arrays;
+import org.bouncycastle.util.BigIntegers;
 
 /**
  * base class for an EdDSA Public Key.
@@ -34,5 +36,16 @@ public class EdDSAPublicBCPGKey
            BigInteger encodedPoint)
     {
         super(oid, encodedPoint);
+    }
+
+    public byte[] getEdDSAEncodedPoint()
+    {
+        BigInteger encodedPoint = getEncodedPoint();
+        byte[] pointData = BigIntegers.asUnsignedByteArray(encodedPoint);
+        if (pointData[0] != 0x40)
+        {
+            throw new IllegalStateException("Invalid point format in EdDSA key!");
+        }
+        return Arrays.copyOfRange(pointData, 1, pointData.length);
     }
 }
